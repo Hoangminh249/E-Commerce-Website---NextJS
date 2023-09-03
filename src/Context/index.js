75c09837fabf0a793e7e129ee36d3246e4b1f02d
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2023-09-03 16:00:32 +0700

    init project

---
+++ src/Context/index.js
@@ -0,0 +1,15 @@
+"use client"
+
+import React, { createContext } from 'react';
+
+export const GlobalContext = createContext(null);
+
+function GlobalState({children}) {
+    return (
+       <GlobalContext.Provider >
+{children}
+       </GlobalContext.Provider>
+    );
+}
+
+export default GlobalState;
